687bb04697662fa8a09fc875878e4e63fe3ff2f1
Jio Im <148739315+JioIm@users.noreply.github.com>
2023-12-08 15:34:12 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,21 @@
 $(function () {
+
+    $(window).on('scroll', function () {
+        const scrollHeight = $(window).scrollTop();
+        if (scrollHeight > 0) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    });
+
     $('.mainVisual .visual_slide').slick({
         pauseOnHover: false,
         dots: true,
-        arrows: false,
-        autoplay: false,
+        arrows: true,
+        autoplay: true,
         autoplaySpeed: 3000
     });
-
-
 
     $('.main_tab li a').on('click', function (e) {
         e.preventDefault();
@@ -20,11 +28,23 @@ $(function () {
     $('.mainProduct .tab_content .tab_slide').slick({
         // lazyLoad: 'ondemand',
         slidesToShow: 4,
-        arrows: false,
+        arrows: true,
         //dots: true,
 
     });
 
+    $('.totop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 800)
+    });
 
-
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 200) {
+            $('.totop').addClass('on');
+            $('.header').addClass('on');
+        } else {
+            $('.totop').removeClass('on');
+            $('.header').removeClass('on');
+        }
+    });
 })
